75b5ad3469dcd4c6500e7bfabb1507eb0a10e7dd
Xhulia Jasimi <ge47waw@mytum.de>
2023-10-17 19:43:09 +0200

    lintt

---
+++ CookHub/CookHub/Views/Helpers/IngredientsAndInstructionsView.swift
@@ -0,0 +1,36 @@
+//
+//  IngredientsAndInstructionsView.swift
+//  CookHub
+//
+//  Created by Xhulia Uni on 17.10.23.
+//
+
+import SwiftUI
+
+struct IngredientsAndInstructionsView: View {
+    @State var localRecipesViewModel: LocalRecipesViewModel
+    var body: some View {
+        Section(header: CustomTextView( text: "Instructions")) {
+            TextEditor(text: $localRecipesViewModel.strInstructions)
+                .frame(minHeight: 200)
+        }
+
+        Section(header: CustomTextView( text: "Ingredients")) {
+            ForEach($localRecipesViewModel.ingredients.indices, id: \.self) { index in
+                IngredientView(ingredient: $localRecipesViewModel.ingredients[index],
+                               amount: $localRecipesViewModel.measurements[index]) {
+                    localRecipesViewModel.ingredients.remove(at: index)
+                    localRecipesViewModel.measurements.remove(at: index)
+                }
+            }
+            if localRecipesViewModel.ingredients.count < 20 {
+                Button(action: {
+                    localRecipesViewModel.ingredients.append("")
+                    localRecipesViewModel.measurements.append("")
+                }, label: {
+                    CustomTextView( text: "Add Ingredient")
+                })
+            }
+        }
+    }
+}
--- CookHub/CookHub/Views/LocalRecipesSubsystem/CreateRecipeView.swift
+++ CookHub/CookHub/Views/LocalRecipesSubsystem/CreateRecipeView.swift
@@ -19,28 +19,7 @@ struct CreateRecipeView: View {
                     TextField("Area", text: $localRecipesViewModel.strArea)
                 }
 
-                Section(header: CustomTextView( text: "Instructions")) {
-                    TextEditor(text: $localRecipesViewModel.strInstructions)
-                        .frame(minHeight: 200)
-                }
-
-                Section(header: CustomTextView( text: "Ingredients")) {
-                    ForEach($localRecipesViewModel.ingredients.indices, id: \.self) { index in
-                        IngredientView(ingredient: $localRecipesViewModel.ingredients[index],
-                                       amount: $localRecipesViewModel.measurements[index]) {
-                            localRecipesViewModel.ingredients.remove(at: index)
-                            localRecipesViewModel.measurements.remove(at: index)
-                        }
-                    }
-                    if localRecipesViewModel.ingredients.count < 20 {
-                        Button(action: {
-                            localRecipesViewModel.ingredients.append("")
-                            localRecipesViewModel.measurements.append("")
-                        }, label: {
-                            CustomTextView( text: "Add Ingredient")
-                        })
-                    }
-                }
+                IngredientsAndInstructionsView(localRecipesViewModel: localRecipesViewModel)
 
                 Section(header: CustomTextView( text: "Thumbnail URL")) {
                     TextField("Thumbnail URL", text: $localRecipesViewModel.strMealThumb)
